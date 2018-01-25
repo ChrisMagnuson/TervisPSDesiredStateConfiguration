87ba3b970b88467f5f5b656de51463357ade6066
ChrisMagnuson <hourglasssand@gmail.com>
2018-01-24 22:56:25 -0500

    Removed uneeded mpclaim code

--- DSCConfiguration/SCDPM2016.ps1
+++ DSCConfiguration/SCDPM2016.ps1
@@ -5,20 +5,6 @@
     Import-DscResource –Module PSDesiredStateConfiguration
 
     Node $Computername {
-        Registry MPIO1 {
-            Ensure = "Present"  # You can also set Ensure to "Absent"
-            Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\MPDEV"
-            ValueType = "Multistring"
-            ValueName = "MPIOSupportedDeviceList"
-            ValueData = "DGC     RAID 3","DGC     RAID 5","DGC     RAID 1","DGC     RAID 0","DGC     RAID 10","DGC     VRAID","DGC     DISK","DGC     LUNZ","DELL    Universal Xport","DELL    MD38xx"
-        }
-        Registry MPIO2 {
-            Ensure = "Present"  # You can also set Ensure to "Absent"
-            Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\msdsm\Parameters"
-            ValueType = "Multistring"
-            ValueName = "DsmSupportedDeviceList"
-            ValueData = "DGC     RAID 3","DGC     RAID 5","DGC     RAID 1","DGC     RAID 0","DGC     RAID 10","DGC     VRAID","DGC     DISK","DGC     LUNZ","DELL    Universal Xport","DELL    MD38xx"
-        }
         Group SCDPMUserAddToAdministratorsGroup {
             GroupName = 'Administrators'
             Ensure = "Present"
