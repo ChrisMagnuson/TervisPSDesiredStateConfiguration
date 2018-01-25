3252b6681db154eead08f1746a11d32c653bd021
ChrisMagnuson <hourglasssand@gmail.com>
2018-01-24 22:52:53 -0500

    Remove uneeded configuration done by Update-TervisSNMPConfiguration

--- DSCConfiguration/SCDPM2016.ps1
+++ DSCConfiguration/SCDPM2016.ps1
@@ -19,20 +19,6 @@
             ValueName = "DsmSupportedDeviceList"
             ValueData = "DGC     RAID 3","DGC     RAID 5","DGC     RAID 1","DGC     RAID 0","DGC     RAID 10","DGC     VRAID","DGC     DISK","DGC     LUNZ","DELL    Universal Xport","DELL    MD38xx"
         }
-        Registry SNMP-PermittedManagers {
-            Ensure = "Present"  # You can also set Ensure to "Absent"
-            Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SNMP\Parameters\PermittedManagers"
-            ValueType = "String"
-            ValueName = "2"
-            ValueData = "orion.tervis.prv"
-        }
-        Registry SNMP-ValidCommunities {
-            Ensure = "Present"  # You can also set Ensure to "Absent"
-            Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\SNMP\Parameters\Validcommunities"
-            ValueType = "DWORD"
-            ValueName = "ttComStr201"
-            ValueData = "4"
-        }
         Group SCDPMUserAddToAdministratorsGroup {
             GroupName = 'Administrators'
             Ensure = "Present"
